7be5dd446ced7c35812f15da7cf14e5b05952b35
jongchan988 <jongchan988@gmail.com>
2022-11-26 21:24:42 +0900

    추가: 깃허브 액션 테스트

---
+++ src/main/java/com/example/action/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.action.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "test hello 2";
+    }
+}
